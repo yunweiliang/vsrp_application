6809cf4a1dfea085ec529e62ca86f241a6f0f702
Yunwei Liang <yunweilia@gmail.com>
2020-06-09 22:18:16 -0700

    add local storage and more specific coordinates

--- flutter_app/vsrp_app/lib/main.dart
+++ flutter_app/vsrp_app/lib/main.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-
+import 'dart:collection';
 import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
@@ -15,29 +15,31 @@ main(){
 class FacePage extends StatefulWidget {
   // Access a Cloud Firestore instance from your Activity
   //final dbReference = Firestore.instance;
+  final localDataCollection = new LipCoordinatesCollection();
 
   @override
-  createState() => _FacePageState();
+  createState() => _FacePageState(dbReference: localDataCollection);
 }
 
 class _FacePageState extends State<FacePage> {
-  //_FacePageState({this.dbReference});
+  _FacePageState({this.dbReference});
   File _imageFile;
   List<Face> _faces;
-  //final dbReference;
+  final dbReference;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text('VSRP: Return Facial Coordinates in Images'),
+        title: Text('VSRP: Lip Coordinates in Images'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _getImageAndDetectFaces,
         tooltip: 'Pick an image',
         child: Icon(Icons.add_a_photo),
       ),
-      body: ImagesAndFaces(imageFile: _imageFile, faces: _faces), //TODO: deal with null _imageFile
+      body: ImagesAndFaces(imageFile: _imageFile,
+          faces: _faces, db: dbReference),
     );
   }
 
@@ -57,7 +59,7 @@ class _FacePageState extends State<FacePage> {
 
     final faces = await faceDetector.detectInImage(image);
 
-    print(faces[0].getLandmark(FaceLandmarkType.bottomMouth).position);
+    // print(faces[0].getLandmark(FaceLandmarkType.bottomMouth).position);
 
     if (mounted){
       setState(() {
@@ -69,12 +71,17 @@ class _FacePageState extends State<FacePage> {
 }
 
 class ImagesAndFaces extends StatelessWidget {
-  ImagesAndFaces({this.imageFile, this.faces});
+  ImagesAndFaces({this.imageFile, this.faces, this.db});
   final File imageFile;
   final List<Face> faces;
+  final LipCoordinatesCollection db;
 
   @override
   Widget build(BuildContext context) {
+
+    if (imageFile == null){
+      return Center(child: Text('Pick a picture.'));
+    }
     //build widget with column format
     return Column(children: <Widget>[
       //display image
@@ -91,7 +98,7 @@ class ImagesAndFaces extends StatelessWidget {
           flex: 1,
           child: ListView(
             // list each face's bounding box coordinates
-            children: faces.map<Widget>((f) => LipCoordinates(f)).toList(),
+            children: faces.map<Widget>((f) => LipCoordinates(f, db)).toList(),
           )
       )
     ],
@@ -100,16 +107,62 @@ class ImagesAndFaces extends StatelessWidget {
 }
 
 class LipCoordinates extends StatelessWidget {
-  LipCoordinates(this.face);
+  LipCoordinates(this.face, this.db);
   final Face face;
+  final LipCoordinatesCollection db;
 
   @override
   Widget build(BuildContext context) {
     final bottomMouthCenter = face.getLandmark(FaceLandmarkType.bottomMouth).position;
+    final leftMouth = face.getLandmark(FaceLandmarkType.leftMouth).position;
+    final rightMouth = face.getLandmark(FaceLandmarkType.rightMouth).position;
+
+
+    if (bottomMouthCenter == null){
+      return ListTile(title:Text('Face not found'));
+    }
+
+    List element = new List(3);
+    element[0] = bottomMouthCenter;
+    element[1] = leftMouth;
+    element[2] = rightMouth;
+    db.recordData(element);
 
     return ListTile(
-      title: Text('Bottom Mouth Center: (${bottomMouthCenter.x},'
-          '${bottomMouthCenter.y})'),
+      title: Text('Bottom Mouth Center: (${bottomMouthCenter.x}, '
+          '${bottomMouthCenter.y}) \n'
+          'Left Mouth: (${leftMouth.x}, ${leftMouth.y} \n'
+          'Right Mouth: (${rightMouth.x}, ${rightMouth.y}'),
     );
   }
+}
+
+
+class LipCoordinatesCollection {
+  List<List> _face_coordinates; // store list of points per picture
+  int i = 0;
+  final numDataType;
+
+  LipCoordinatesCollection({int size = 10, this.numDataType = 6}){
+    _face_coordinates = List(size);
+  }
+
+  void recordData(face_coordinates){
+    print(_face_coordinates);
+    _face_coordinates[i] = face_coordinates;
+    i++;
+  }
+
+  /*List getCurrent(){
+    i++;
+    try {
+      return _face_coordinates[i] = new List(numDataType);
+    } catch (e){
+      print(e);
+    }
+  }
+
+  void setCurrent(int index, int num){
+
+  }*/
 }
