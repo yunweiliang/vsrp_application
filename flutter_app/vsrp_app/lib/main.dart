72993662a02305973cc8b01d68255699642f0c3d
Yunwei Liang <yunweilia@gmail.com>
2020-06-09 20:37:13 -0700

    produce bottom lip coordinates

--- flutter_app/vsrp_app/lib/main.dart
+++ flutter_app/vsrp_app/lib/main.dart
@@ -4,6 +4,8 @@ import 'package:firebase_ml_vision/firebase_ml_vision.dart';
 import 'package:flutter/material.dart';
 import 'package:image_picker/image_picker.dart';
 
+//import 'package:cloud_firestore/cloud_firestore.dart';
+
 main(){
   runApp(MaterialApp(
     home: FacePage(),
@@ -11,21 +13,24 @@ main(){
 }
 
 class FacePage extends StatefulWidget {
+  // Access a Cloud Firestore instance from your Activity
+  //final dbReference = Firestore.instance;
+
   @override
   createState() => _FacePageState();
 }
 
 class _FacePageState extends State<FacePage> {
+  //_FacePageState({this.dbReference});
   File _imageFile;
   List<Face> _faces;
-
+  //final dbReference;
 
   @override
   Widget build(BuildContext context) {
-    print('_imageFile type in FacePage Widget build: $_imageFile');
     return Scaffold(
       appBar: AppBar(
-        title: Text('VSRP Face Detector'),
+        title: Text('VSRP: Return Facial Coordinates in Images'),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: _getImageAndDetectFaces,
@@ -40,16 +45,20 @@ class _FacePageState extends State<FacePage> {
     final imageFile = await ImagePicker.pickImage(
       source: ImageSource.gallery,
     );
-    print('_getImageAndDetectFaces() ran--------------------------------');
     final image = FirebaseVisionImage.fromFile(imageFile);
+
     final faceDetector = FirebaseVision.instance.faceDetector(
       FaceDetectorOptions(
         mode: FaceDetectorMode.accurate,
         enableLandmarks: true,
+        enableClassification: true,
       ),
     );
 
     final faces = await faceDetector.detectInImage(image);
+
+    print(faces[0].getLandmark(FaceLandmarkType.bottomMouth).position);
+
     if (mounted){
       setState(() {
         _imageFile = imageFile;
@@ -66,7 +75,6 @@ class ImagesAndFaces extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    print('imageFile type: $faces');
     //build widget with column format
     return Column(children: <Widget>[
       //display image
@@ -79,12 +87,11 @@ class ImagesAndFaces extends StatelessWidget {
               fit: BoxFit.cover,
             ),
           )),
-      //display bounding box in list
       Flexible(
           flex: 1,
           child: ListView(
             // list each face's bounding box coordinates
-            children: faces.map<Widget>((f) => FaceCoordinates(f)).toList(),
+            children: faces.map<Widget>((f) => LipCoordinates(f)).toList(),
           )
       )
     ],
@@ -92,134 +99,17 @@ class ImagesAndFaces extends StatelessWidget {
   }
 }
 
-class FaceCoordinates extends StatelessWidget {
-  FaceCoordinates(this.face);
+class LipCoordinates extends StatelessWidget {
+  LipCoordinates(this.face);
   final Face face;
 
   @override
   Widget build(BuildContext context) {
-    final pos = face.boundingBox;
+    final bottomMouthCenter = face.getLandmark(FaceLandmarkType.bottomMouth).position;
+
     return ListTile(
-      title: Text('(${pos.top}, ${pos.left}, ${pos.bottom}, ${pos.right}'),
+      title: Text('Bottom Mouth Center: (${bottomMouthCenter.x},'
+          '${bottomMouthCenter.y})'),
     );
   }
 }
-
-
-
-
-
-
-/*
-import 'dart:async';
-import 'dart:io';
-
-import 'package:camera/camera.dart';
-import 'package:flutter/material.dart';
-import 'package:path/path.dart' show join;
-import 'package:path_provider/path_provider.dart';
-
-
-Future<void> main() async{
-  WidgetsFlutterBinding.ensureInitialized();
-  final cameras = await availableCameras();
-  final firstCamera = cameras.first;
-
-  runApp(MaterialApp(
-    home: CameraPage(
-      camera: firstCamera,
-    ),
-  ));
-}
-
-class CameraPage extends StatefulWidget{
-  final CameraDescription camera;
-
-  CameraPage({
-    @required this.camera
-  });
-
-  @override
-  CameraPageState createState() => CameraPageState();
-}
-
-class CameraPageState extends State<CameraPage>{
-  CameraController _controller;
-  Future<void> _initializeControllerFuture;
-
-  @override
-  void initState(){
-    super.initState();
-
-    _controller = CameraController(
-        widget.camera,
-        ResolutionPreset.medium);
-
-    _initializeControllerFuture = _controller.initialize();
-  }
-
-  @override
-  void dispose(){
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(title: Text('Take a picture'),),
-      body: FutureBuilder<void>(
-        future: _initializeControllerFuture,
-        builder: (context, snapshot) {
-          if (snapshot.connectionState == ConnectionState.done){
-            return CameraPreview(_controller);
-          } else {
-            return Center(child: CircularProgressIndicator());
-          }
-        },
-      ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.camera_alt),
-        onPressed: () async {
-          try{
-            await _initializeControllerFuture;
-            final path = join(
-                (await getTemporaryDirectory()).path,
-                '${DateTime.now()}.png'
-            );
-
-            await _controller.takePicture(path);
-
-            Navigator.push(
-              context,
-              MaterialPageRoute(
-                builder: (context) => DisplayPicturePage(imagePath: path),
-              ),
-            );
-          } catch (e) {
-            print(e);
-          }
-        },
-      ),
-    );
-  }
-}
-
-class DisplayPicturePage extends StatelessWidget {
-  final String imagePath;
-
-  const DisplayPicturePage({Key key, this.imagePath}): super(key: key);
-
-  @override
-  Widget build(BuildContext context) {
-    // TODO: implement build
-    return Scaffold(
-      appBar: AppBar(title: Text('Picture')),
-      body: Image.file(File(imagePath)),
-    );
-  }
-}
-
-
-*/
